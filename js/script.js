d328277e92d3d6502105307c4932d7c853e4a5e7
Long N <longn5@gmail.com>
2018-04-02 15:54:55 -0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+setTimeout(function(){
+  document.getElementById("vid").play();
+}, 1500);
+
+// setTimeout(function(){
+//   document.getElementById("background-music").play();
+// }, 300);
+
+// var $video = $('video'),
+//     $window = $(window);
+//
+// $(window).resize(function (){
+//
+//   var height = $window.height();
+//   $video.css('height', height);
+//
+//   var videoWidth = $video.width(),
+//       windowWidth = $window.width(),
+//       marginLeftAdjust = (windowWidth - videoWidth) / 2;
+//
+//   $video.css({
+//     'height' : height;
+//     'marginLeft' : marginLeftAdjust;
+//   });
+// }).resize;
